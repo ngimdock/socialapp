b6e8d45d8324bdb5bdd9521649ad34aacf9c11dd
ngimdock <ngimdockzemfack@gmail.com>
2021-07-18 15:02:17 +0100

    implementation du  menu responsive

--- src/components/Publication.jsx
+++ src/components/Publication.jsx
@@ -4,18 +4,34 @@ import Avatar from './avatar';
 function Publication({pubInfo}) {
 
     const [numberOfChat, setNumberOfChat] = useState(0);
+    const [numberOfTweet, setNumberOfTweet] = useState(0);
+    const [numberOfLike, setNumberOfLike] = useState(0);
+    const [numberOfShare, setNumberOfShare] = useState(0);
+
+    const handleSetNumberOfChat = () => {
+        setNumberOfChat(numberOfChat+1);
+    };
+
+    const handleSetNumberOfTweet = () => {
+        setNumberOfTweet(numberOfTweet+1);
+    };
+
+    const handleSetNumberOfLike = () => {
+        setNumberOfLike(numberOfLike+1);
+    };  
+
     const getPseudo = (name) => {
         const randomNumber = Math.floor(Math.random()*100) +1;
         return ` @${name}${randomNumber}`; 
     }
 
-function formatDate(date) {
-    const day = date.getDate();
-    const month = date.getMonth();
-    const year = date.getFullYear();
+    function formatDate(date) {
+        const day = date.getDate();
+        const month = date.getMonth();
+        const year = date.getFullYear();
 
-    return `${day} / ${month+1} / ${year}`
-}
+        return `${day} / ${month+1} / ${year}`
+    }
 
     return (
         <article className="publication-item">
@@ -42,7 +58,16 @@ function formatDate(date) {
                     <p>
                         {pubInfo.text}
                     </p>
-                    <PublicationInfo />
+                    <PublicationInfo
+                        numberOfChat={numberOfChat}
+                        onNumberOfChatChange={handleSetNumberOfChat}
+                        numberOfTweet={numberOfTweet}
+                        onNumberOfTweetChange={handleSetNumberOfTweet}
+                        numberOfLike={numberOfLike}
+                        onNumberOfLikeChange={handleSetNumberOfLike}
+                        numberOfShare={numberOfShare}
+                        onNumberOfShareChange={()=> setNumberOfShare(numberOfShare+1)}
+                    />
                 </main>
             </section>
         </article>
@@ -51,12 +76,34 @@ function formatDate(date) {
 
 
 function PublicationInfo(props) {
+    const {
+            numberOfChat,
+            onNumberOfChatChange,
+            numberOfTweet, 
+            onNumberOfTweetChange,
+            numberOfLike,
+            onNumberOfLikeChange,
+            numberOfShare,
+            onNumberOfShareChange
+          } = props;
     return (
         <ul className="publication-info">
-            <li><i className="bi bi-chat-left"></i> <span>23</span> </li>
-            <li><i className="bi bi-bounding-box-circles"></i>  <span>23</span></li>
-            <li><i className="bi bi-suit-heart"></i>  <span>23</span></li>
-            <li><i className="bi bi-reply"></i>  <span>23</span></li>
+            <li>
+                <i className="bi bi-chat-left" onClick={onNumberOfChatChange}></i> 
+                <span>{numberOfChat}</span> 
+            </li>
+            <li>
+                <i className="bi bi-bounding-box-circles" onClick={onNumberOfTweetChange}></i>  
+                <span>{numberOfTweet}</span>
+            </li>
+            <li>
+                <i className="bi bi-suit-heart" onClick={onNumberOfLikeChange}></i>  
+                <span>{numberOfLike}</span>
+            </li>
+            <li>
+                <i className="bi bi-reply" onClick={onNumberOfShareChange}></i>  
+                <span>{numberOfShare}</span>
+            </li>
         </ul>
     );
 }
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,10 @@
+import { Button } from 'react-bootstrap';
 
 function Footer(props) {
 
     return(
         <footer className="footBar">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate doloremque fuga voluptatibus corrupti sed asperiores soluta dolorum, quibusdam at! Molestiae provident laboriosam sint optio nesciunt aut nostrum, nisi eum ex.
+            made by d@n
         </footer>
     );
 }
